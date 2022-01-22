755f7a2e8ea6901c01cecaf85a6ec8b5d43e433d
rohit20001221 <rohit20001221@gmail.com>
2022-01-23 00:34:44 +0530

    feat(app): fetch the strategies from the rest api

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 import time
-
+import requests
+import json
+import time
 
 from entities.kite_ticker import KiteLiveDataServer
 from entities.redis_db import DB
@@ -9,11 +11,28 @@ from threading import Thread
 
 API_KEY = os.environ["API_KEY"]
 ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+BITTRADE_HOST = os.environ["BITTRADE_HOST"]
+AUTH_TOKEN = os.environ["AUTH_TOKEN"]
 
 db = DB()
 ticker = KiteLiveDataServer(api_key=API_KEY, access_token=ACCESS_TOKEN, db=db)
 
 ticker.start()
+time.sleep(5)
+
+strategies = requests.get(
+    BITTRADE_HOST + "/strategy_builder/get_strategies",
+    headers={"Authorization": f"Token {AUTH_TOKEN}"},
+).json()
+
+token_set = set()
+
+for strategy in strategies:
+    for _ticker in strategy["strategy_tickers"]:
+        token_set.add(_ticker["instrument_token"])
+
+ticker.subscribe(list(token_set))
+ticker.set_mode(ticker.MODE_FULL, list(token_set))
 
 
 def entry_service():
